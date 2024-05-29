42b342aaea97b16654697711ae0f75be1a280bea
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-05-29 14:40:04 +0700

    update

--- src/app/controllers/Authenticate_Controller.js
+++ src/app/controllers/Authenticate_Controller.js
@@ -7,27 +7,30 @@ class Authenticate_Controller {
     const username = req.body.username;
     const password = req.body.password;
     const user = { name: username };
-
-    var client = ldap.createClient({
-      url: process.env.LDAP_URI,
-      timeout: 5000,
-      connectTimeout: 10000,
-    });
-    client.bind(username, password, (err) => {
-      client.unbind();
-      if (err) {
-        console.log(err);
-        res.sendStatus(401);
-      } else {
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: "10d",
-        });
-        res.json({
-          accessToken: accessToken,
-          username: username,
-        });
-      }
-    });
+    if (username && password) {
+      var client = ldap.createClient({
+        url: process.env.LDAP_URI,
+        timeout: 5000,
+        connectTimeout: 10000,
+      });
+      client.bind(username, password, (err) => {
+        client.unbind();
+        if (err) {
+          console.log(err);
+          res.sendStatus(401);
+        } else {
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "10d",
+          });
+          res.json({
+            accessToken: accessToken,
+            username: username,
+          });
+        }
+      });
+    } else {
+      res.json({ message: "Please Provide User And Password" });
+    }
   }
 }
 
