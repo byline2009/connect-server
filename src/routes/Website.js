79517040675e4216c310335c0c869e67d6689f06
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-10-29 14:55:02 +0700

    up

--- src/routes/Website.js
+++ src/routes/Website.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const websiteController = require("../app/controllers/Website_Controller");
 router.use("/package", websiteController.getPackage);
-router.use("/type", websiteController.checkType);
+router.use("/type", websiteController.getType);
 router.use("/", websiteController.index);
 
 module.exports = router;
